cbdf59f9d329a879f560fdd140772c140226e741
沈赟杰 <shenyunjie@znrank.com>
2023-09-22 18:10:49 +0800

    update

---
+++ src/theme/error.ts
@@ -0,0 +1,4 @@
+export type Props = {
+  status: number,
+  message: string,
+}
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,7 @@
 export * as Home from './home';
 export * as Articles from './articles';
 export * as Article from './article';
+export * as Forbiden from './forbiden';
+export * as Close from './close';
+export * as NotFound from './notfound';
+export * as ServerError from './error';
